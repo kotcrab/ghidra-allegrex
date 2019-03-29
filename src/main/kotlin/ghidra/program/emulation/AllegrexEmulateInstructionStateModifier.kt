4e3f3cfb585d7d346c229c307128ded2693d9e46
Paweł Pastuszak <kotcrab@gmail.com>
2019-03-29 18:58:08 +0100

    Clean up

--- src/main/kotlin/ghidra/program/emulation/AllegrexEmulateInstructionStateModifier.kt
+++ src/main/kotlin/ghidra/program/emulation/AllegrexEmulateInstructionStateModifier.kt
@@ -7,52 +7,8 @@ import ghidra.pcode.emulate.callother.CountLeadingZerosOpBehavior
 
 class AllegrexEmulateInstructionStateModifier(emu: Emulate) : EmulateInstructionStateModifier(emu) {
     init {
+        // Refer to allegrex.sinc for additional PcodeOp that can be implemented
         registerPcodeOpBehavior("countLeadingZeros", CountLeadingZerosOpBehavior())
         registerPcodeOpBehavior("countLeadingOnes", CountLeadingOnesOpBehavior())
-
-        //We could registerPcodeOpBehavior for one or more of the following pcodeop's:
-        //break;
-        //trap;
-        //wait;
-        //syscall;
-        //cacheOp;
-        //signalDebugBreakpointException;
-        //disableInterrupts;
-        //enableInterrupts;
-        //hazzard;
-        //lockload;
-        //lockwrite;
-        //synch;
-        //tlbop;
-        //bitSwap;
-        //disableProcessor;
-        //enableProcessor;
-        //signalReservedInstruction;
-        //prefetch;
-        //getFpCondition;
-        //getCopCondition;
-        //setCopControlWord;
-        //getCopControlWord;
-        //copFunction;
-        //getCopReg;
-        //getCopRegH;
-        //setCopReg;
-        //setCopRegH;
-        //extractField;
-        //insertField;
-        //getHWRegister;
-        //setShadow;
-        //getShadow;
-        //special2;
-        //SYNC;
-        //TLB_invalidate;
-        //TLB_invalidate_flush;
-        //TLB_probe_for_matching_entry;
-        //TLB_read_indexed_entryHi;
-        //TLB_read_indexed_entryLo0;
-        //TLB_read_indexed_entryLo1;
-        //TLB_read_indexed_entryPageMask;
-        //TLB_write_indexed_entry;
-        //TLB_write_random_entry;
     }
 }
