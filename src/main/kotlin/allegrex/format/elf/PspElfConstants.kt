884d8571ea3ac606b4e9e106ac1ba208cd5f9d1b
kotcrab <4594081+kotcrab@users.noreply.github.com>
2024-08-31 12:16:59 +0200

    Use const

--- src/main/kotlin/allegrex/format/elf/PspElfConstants.kt
+++ src/main/kotlin/allegrex/format/elf/PspElfConstants.kt
@@ -1,7 +1,6 @@
 package allegrex.format.elf
 
-@Suppress("MayBeConstant")
 object PspElfConstants {
-  val SHT_PSP_REL = 0x700000A0
-  val SHT_PSP_REL_TYPE_B = 0x700000A1
+  const val SHT_PSP_REL = 0x700000A0
+  const val SHT_PSP_REL_TYPE_B = 0x700000A1
 }
